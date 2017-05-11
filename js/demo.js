cfeb300a6072f83df115adba93524c19fd876f71
Andy Day <andy.day@nordstrom.com>
2017-05-10 22:50:02 -0700

    initial

---
+++ js/demo.js
@@ -0,0 +1,36 @@
+
+const express = require('express')
+const rp = require('request-promise')
+const app = express()
+const tracer = require('ctrace-js')
+const tr = tracer.request
+
+tr.init(rp)
+
+app.use(tracer.express())
+
+function send (ctx, uri, region) {
+  return tr({method: 'GET', url: `${uri}?region=${region}`})
+}
+
+app.get('/gw', (req, res) => {
+  send(req.traceContext, decodeURI(req.query.url), decodeURI(req.query.region))
+    .then(rtn => {
+      res.send(rtn)
+    })
+    .catch(e => {
+      res.status(500).send(e.err || e)
+    })
+})
+
+app.get('/ok', (req, res) => {
+  let span = req.traceContext.span
+  let region = req.query.region
+  let msg = `Hello ${region}!`
+  span.log({event: 'generate-msg', message: msg})
+  res.send(msg)
+})
+
+app.listen(80, function () {
+  console.log('Hello Gateway Started on port 80...')
+})
